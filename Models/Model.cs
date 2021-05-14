ef6c7eedfb5aa8b651da931e6d647667613de9be
speed <speed@DESKTOP-GBB5EQH>
2021-05-14 11:55:25 +0300

    add model attribute

---
+++ Models/Model.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication13.Model
+{
+    public class Model
+    {
+
+        public int Id { get; set; }
+        public string UploadTime { get; set; }
+        public String Status { get; set; }
+
+
+    }
+}
