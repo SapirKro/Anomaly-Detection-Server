35438f8d61da49903fe47dbd76f54de614259a72
speed <speed@DESKTOP-GBB5EQH>
2021-05-14 11:51:24 +0300

    change to json

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
+using System.Net.Http.Headers;
 namespace WebApplication13
 {
     public static class WebApiConfig
@@ -13,6 +14,8 @@ namespace WebApplication13
             //SAPIR
             // Web API routes
             config.MapHttpAttributeRoutes();
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
