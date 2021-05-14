a71f58ab63836f4dbf6ad2dea94b08be0c3c41db
speed <speed@DESKTOP-GBB5EQH>
2021-05-14 11:56:53 +0300

    add model interface

---
+++ Models/Interface1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication13.Models
+{
+    interface IModelsManager
+    {
+
+        IEnumerable<Model> GetAllModels();
+        Model GetModelById(int id);
+        void AddModel(Model p);
+        void UpdateModel(Model p);
+        void DeleteModel(int id);
+    }
+}
