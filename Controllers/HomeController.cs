674c664c0d8c6e73c4e72787135ac224c218bae7
speed <speed@DESKTOP-GBB5EQH>
2021-05-25 15:48:59 +0300

    usuing ajax

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,15 +23,21 @@ namespace WebApp.Controllers
 
             return View();
         }
+		public ActionResult About1()
+		{
+			ViewBag.HTMLCheck = true;
+			ViewBag.Message = "The app made by:Sapir Kroitoro ,Omer Mazal,Amichai Vollin,Ben Rotlevi.";
 
-       /* public ActionResult Contact()
-        {
-            ViewBag.Message1 = "Your contact page.1";
-            
-            ex1main e1 = new ex1main(35);
-            int num = e1.mymain();
-            ViewBag.Message2 = num;
-            return View();
-        }*/
-    }
+			return View();
+		}
+		/* public ActionResult Contact()
+		 {
+			 ViewBag.Message1 = "Your contact page.1";
+
+			 ex1main e1 = new ex1main(35);
+			 int num = e1.mymain();
+			 ViewBag.Message2 = num;
+			 return View();
+		 }*/
+	}
 }
